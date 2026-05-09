353de4cb712c9ca34fef6f8cd928b3ae57bd44a9
mohitmakavana70-star <mohitmakavana70@gmail.com>
2026-05-09 12:12:11 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: isProduction
+    ? "https://team-task-management-system-u8ii.onrender.com/api"
+    : "http://localhost:5000/api"
 });
 
 api.interceptors.request.use((config) => {
