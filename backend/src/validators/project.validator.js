462611bc119994abbe1ea1547b59b9dc114b0dc7
mohitmakavana70-star <mohitmakavana70@gmail.com>
2026-05-09 12:00:58 +0530

    Delete backend directory

--- backend/src/validators/project.validator.js
+++
@@ -1,7 +0,0 @@
-import { body } from "express-validator";
-
-export const projectValidator = [
-  body("name").notEmpty().withMessage("Project name is required"),
-  body("description").optional().isString(),
-  body("members").optional().isArray().withMessage("Members must be an array")
-];
