8e6a61de6bf6f49b1b160ed313f994782b970d70
mohitmakavana70-star <mohitmakavana70@gmail.com>
2026-05-09 10:29:46 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,7 +29,7 @@ app.use(
 
 app.use(
   cors({
-    origin:[ "http://localhost:5173", "https://team-task-management-system-3xht-efprwimx2.vercel.app" ],
+    origin:[ "http://localhost:5173", "https://team-task-management-system-3xht.vercel.app" ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
